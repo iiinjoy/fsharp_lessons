a4d1711c9f04b5b49d30bf17817a6ea83d88c664
Aynur Ishbuldin <iiinjoy@gmail.com>
2020-02-15 15:18:56 +0500

    fix lesson10

--- lesson10.fs
+++ lesson10.fs
@@ -1,6 +1,12 @@
 type TimeOfDay = { hours: int; minutes: int; f: string }
 
 let (.>.) x y =
-    let {hours = h1; minutes = m1; f = f1} = x
-    let {hours = h2; minutes = m2; f = f2} = y
-    f1 > f2 || h1 > h2 || m1 > m2
+    let to_minutes {hours = hh; minutes = mm; f = ff} =
+        if ff = "AM" then (hh * 60 + mm) % (24*60)
+        else ((hh + 12) * 60 + mm) % (24*60)
+    let from_minutes m =
+            let ff = if m >= (12*60) then "PM" else "AM"
+            let hh = (m % (12*60)) / 60
+            let mm = (m % (12*60)) - hh * 60
+            { hours = hh; minutes = mm; f = ff }
+    (to_minutes x) > (to_minutes y)
